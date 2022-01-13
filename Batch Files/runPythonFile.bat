acf4a479b564861fb869b32229c11e662afb7a89
huijuank <61876615+huijuank@users.noreply.github.com>
2022-01-13 18:39:13 +0800

    Create runPythonFile.bat

---
+++ Batch Files/runPythonFile.bat
@@ -0,0 +1,6 @@
+@echo Activating python environment...
+@Call "C:/Users/<user>/Anaconda3/Scripts/activate.bat"
+@call conda activate <envname>
+@echo Activating <envname>...
+@python "filename.py"
+pause
